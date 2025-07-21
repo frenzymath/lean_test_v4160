d714810bda5785d587d32bd85bb8201658d80b8d
slashbade <slashbladevitali@gmail.com>
2025-07-21 12:31:06 +0800

    feat: add Test and Temp dir, fix: remove REPL

--- lakefile.lean
+++ lakefile.lean
@@ -11,8 +11,5 @@ lean_lib «LeanTestV4160» where
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4" @ "v4.16.0"
 
-require REPL from git
-  "https://github.com/leanprover-community/repl.git" @ "v4.16.0"
-
 require jixia from git
   "https://github.com/frenzymath/jixia.git" @ "v4.16.0"
